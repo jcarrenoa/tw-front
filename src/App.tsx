f1d4c6c4eec029c25030bb76af9b5a825e5a685a
jcarrenoa <jcarrenoa@uninorte.edu.co>
2025-04-20 15:32:53 -0500

    feat: dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Public from '@components/public/index'
 import Private from '@components/private/index'
 import './App.css'
@@ -6,9 +6,27 @@ import './App.css'
 export function App() {
   const [login, setLogin] = useState(true);
   const [user, setUser] = useState({});
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme") === "dark";
+  });
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
   
   return (
     <>
+    <div className="container">
+      <button className="toggle-btn" onClick={() => setDarkMode(!darkMode)}>
+        {darkMode ? <i className="fas fa-sun"></i>: <i className="fas fa-moon"></i> }
+      </button>
+    </div>
       { login ? 
         <Private/>
       :
