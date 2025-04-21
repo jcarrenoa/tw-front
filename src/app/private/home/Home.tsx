a7d4b7a85fad61e9c5427440635768e2394d2958
jcarrenoa <jcarrenoa@uninorte.edu.co>
2025-04-21 13:20:18 -0500

    feat: functional home page

--- src/app/private/home/Home.tsx
+++ src/app/private/home/Home.tsx
@@ -6,7 +6,9 @@ import logo_n from '@media/x_negro.png'
 import logo_b from '@media/x_blanco.png'
 import { useEffect, useState } from 'react'
 import { allUsers } from '@http/user'
-import { postUser } from '@http/tweets'
+import { postUser, createTweet } from '@http/tweets'
+import { useMemo } from "react";
+
 
 interface type {
   mode: boolean
@@ -35,22 +37,39 @@ function Home({ mode, user }: type) {
       "createdAt": "2025-04-21T07:00:41.987+00:00"
     }])
 
+    const suggestedUsers = useMemo(() => {
+      return [...users]
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 3);
+    }, [users]);
+
+    const [tweet, setTweet] = useState("");
+
     const logo = mode ? logo_b : logo_n
 
+    const fetchUsers = async () => {
+      const aux = await allUsers();
+      setUsers(aux);
+    };
+    
+    const fetchPosts = async () => {
+      const aux = await postUser();
+      setPosts(aux);
+    };
+    
     useEffect(() => {
-      const user = async() => {
-        const aux = await allUsers()
-        setUsers(aux)
-      } 
-      const post = async() => {
-        const aux = await postUser()
-        setPosts(aux)
-      }
-      user()
-      post()
-    }, [])
+      fetchUsers();
+      fetchPosts();
+    }, []);
+    
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      await createTweet(tweet);
+      setTweet("");
+      fetchUsers();
+      fetchPosts();
+    };
 
-    console.log(posts)
 
     return (
       <main className={ICSS['container']}>
@@ -70,10 +89,20 @@ function Home({ mode, user }: type) {
           <div className={`${ICSS["tweet-box"]} ${ICSS['item']}`}>
             <img className={ICSS['img']} src={user ? `https://unavatar.io/${user.username}` : `https://unavatar.io/unknow`} alt="" />
             <div className={ICSS["tweet-input-container"]}>
-              <textarea placeholder="¿Qué está pasando?" className={ICSS["tweet-input"]}></textarea>
-              <div className={ICSS["tweet-actions"]}>
-                <button className={ICSS["btn"]}>Publicar</button>
-              </div>
+              <form onSubmit={handleSubmit}>
+                <textarea
+                  placeholder="¿Qué está pasando?"
+                  className={ICSS["tweet-input"]}
+                  value={tweet}
+                  onChange={(e) => setTweet(e.target.value)}
+                ></textarea>
+
+                <div className={ICSS["tweet-actions"]}>
+                  <button className={ICSS["btn"]} type="submit">
+                    Publicar
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
           <div className={`${ICSS['posts']} ${ICSS['item']}`}>
@@ -89,9 +118,7 @@ function Home({ mode, user }: type) {
         <section className={`${ICSS['info-container']} ${ICSS['item']}`}>
           <h2>Personas para seguir</h2>
           <div className={ICSS['follows-cards']}>
-            {[...users]
-			.sort(() => Math.random() - 0.5)
-			.slice(0, 3).map((user) => {
+            {suggestedUsers.map((user) => {
               return (
                 <FollowCard key={user._id} userName={user.username} initialIsFollowing={false}>
                   {user.name}
