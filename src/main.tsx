b8dc54f8f202fa4efba15b3a83650b527b969355
jcarrenoa <jcarrenoa@uninorte.edu.co>
2025-05-18 12:31:04 -0500

    feat: the application was configured with the Sentry platform

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
+import * as Sentry from "@sentry/react";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router';
+
+Sentry.init({
+  dsn: "https://ce3ed47e85350b09e3f776221b9e7d45@o4509345132707840.ingest.us.sentry.io/4509345135263744",
+  sendDefaultPii: true
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
